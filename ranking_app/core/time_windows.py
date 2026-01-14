2677a0be1d6457f66746e25d3903922c50b07311
nilesh sriram <123529775+neelu369@users.noreply.github.com>
2026-01-14 12:17:39 +0530

    Delete ranking_app directory
    Adding another better ranking app

--- ranking_app/core/time_windows.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def slice_time_window(df, start_date, end_date):
-    start_ts = pd.to_datetime(start_date)
-    end_ts = pd.to_datetime(end_date)
-
-    mask = (df["order_date"] >= start_ts) & (df["order_date"] <= end_ts)
-    return df.loc[mask].copy()
